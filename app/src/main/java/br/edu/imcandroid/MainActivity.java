345b255475924ca429457921d1e6398d4cf3937c
Álisson Gomes Linhares <linhares.alisson@gmail.com>
2016-04-06 21:15:54 -0300

    Com imageView e padding no botão Calcular IMC

--- app/src/main/java/br/edu/imcandroid/MainActivity.java
+++ app/src/main/java/br/edu/imcandroid/MainActivity.java
@@ -4,33 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextAltura;
+    private EditText editTextPeso;
+    private EditText editTextResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextAltura = (EditText) findViewById(R.id.editTextAltura);
+        editTextPeso = (EditText) findViewById(R.id.editTextPeso);
+        editTextResultado = (EditText) findViewById(R.id.editTextResultado);
     }
 
     public void calcularImc (View v){
-
         // ENTRADA
-        double peso = Double.parseDouble(((EditText) findViewById(R.id.editTextPeso)).getText().toString());
+        String s_altura = editTextAltura.getText().toString();
+        String s_peso = editTextPeso.getText().toString();
 
-        EditText editTextAltura = (EditText) findViewById(R.id.editTextAltura);
-        String altura_s = editTextAltura.getText().toString();
-        double altura = Double.parseDouble(altura_s);
+        // CONVERSAO DE STRING PARA DOUBLE
+        double altura = Double.parseDouble(s_altura);
+        double peso = Double.parseDouble(s_peso);
 
         // PROCESSAMENTO
         double imc = peso / (altura * altura);
 
         // SAIDA
-        EditText editTextResultado = (EditText) findViewById(R.id.editTextResultado);
         editTextResultado.setText(imc+"");
     }
-
 }
 
